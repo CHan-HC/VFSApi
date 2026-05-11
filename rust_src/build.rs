5ceef39e27f8e3d33edbc07dcc9295713151b886
CHan <124427703@qq.com>
2026-05-11 19:25:41 +0800

    build.rs中SDK路径改用HARMONY_SDK_HOME环境变量，提高可移植性

--- rust_src/build.rs
+++ rust_src/build.rs
@@ -1,5 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=/Applications/DevEco-Studio.app/Contents/sdk/default/hms/native/sysroot/usr/lib/aarch64-linux-ohos");
+    let sdk_path = std::env::var("HARMONY_SDK_HOME")
+        .unwrap_or_else(|_| "/Applications/DevEco-Studio.app/Contents/sdk/default".to_string());
+    println!("cargo:rustc-link-search=native={}/hms/native/sysroot/usr/lib/aarch64-linux-ohos", sdk_path);
     println!("cargo:rustc-link-lib=ace_napi.z");
     println!("cargo:rustc-link-lib=hilog_ndk.z");
     println!("cargo:rustc-link-lib=rcp_c");
